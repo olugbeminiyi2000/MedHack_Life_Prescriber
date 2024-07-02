fdae771e2b3787c13911479a6a1f7282d2ec5981
akumuyi <a.kumuyi@alustudent.com>
2024-07-02 12:11:42 +0200

    More styling

--- Life_Prescriber/Life_Prescriber/settings.py
+++ Life_Prescriber/Life_Prescriber/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -163,3 +163,7 @@ EMAIL_PORT = 587
 # the deafult user model called User.
 AUTH_USER_MODEL = "prescription_ongo.Patient"
 
+#static Files Directories
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'prescription_ongo', 'static'),
+]
