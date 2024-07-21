0e62e38d61cf5159292519018306f2ee7432339e
Obolo Emmanuel <e.obolo@alustudent.com>
2024-07-21 21:59:57 +0200

    new updates search implemented

--- Life_Prescriber/search.js
+++
@@ -1,8 +0,0 @@
-const searchButton = document.querySelector("#search-button");
-searchButton.addEventListener("click", (eventObject) => {
-    const dataUrl = eventObject.currentTarget.getAttribute("data-url");
-    const searchBox = document.querySelector("#search-box");
-    const searchQuery = searchBox.textContent;
-    const searchDataUrl = `${dataUrl}?search=${searchQuery}`;
-    window.location.href = searchDataUrl;
-})
